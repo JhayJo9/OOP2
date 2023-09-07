f0a3d95b7891ce4df8863a16912b99b87136f425
JhayJo9 <penalozajhay8@gmail.com>
2023-09-07 02:53:37 -0700

    creating a class,

---
+++ src/main/java/itec30/java_oop_maven3/Java_oop_maven3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package itec30.java_oop_maven3;
+
+/**
+ *
+ * @author JhayJ 
+ */
+public class Java_oop_maven3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
